0f00a028fed35d1af5efe22f1d2a263892dd34c2
tib57 <juliette.tib@gmail.com>
2015-03-03 22:04:32 +0100

    Résolution de conflits

--- ProjetLog/src/Fait.java
+++ ProjetLog/src/Fait.java
@@ -1,9 +1,11 @@
+import java.util.Vector;
+
 
 public class Fait {
-	
+
 	private String name;
 	private double[] data;
-	
+
 	public Fait(String nom, double[] donnees){
 		this.name = nom;
 		this.data = donnees;
@@ -15,5 +17,23 @@ public class Fait {
 	
 	public String getName(){
 		return this.name;
+	}
+	
+	public Vector<Pair<Integer,Integer>> aEffectuer(Vector<Regle> rules){
+		boolean[] b;
+		Vector<Pair<Integer,Integer>> listeActions = new Vector<Pair<Integer,Integer>>();
+		for(Regle r : rules){
+			Fait fait = r.getFait();
+			if(fait == this){
+				b = (r.getCondition().eval(this));
+				for(int i=0; i<data.length; i++){
+					if(b[i]){
+						Pair<Integer,Integer> paire = new Pair<Integer,Integer>(i,r.getAction());
+						listeActions.add(paire);
+					}
+				}
+			}
+		}
+		return listeActions;
 	}
 }
