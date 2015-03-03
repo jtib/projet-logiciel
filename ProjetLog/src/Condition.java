dcfb1670fbf8ed1382226dda02dce2b259a3f232
tib57 <juliette.tib@gmail.com>
2015-03-03 20:27:20 +0100

    Commentaire inutile supprimé

--- ProjetLog/src/Condition.java
+++ ProjetLog/src/Condition.java
@@ -9,11 +9,11 @@ public class Condition {
 		this.comparaisons = compa;
 	}
 	
-	//ajouter méthode pour ajouter comparaison au vecteur comparaisons
+	//ajouter m≈Ωthode pour ajouter comparaison au vecteur comparaisons
 	
-	/** Méthode pour évaluer une comparaison
+	/** M≈Ωthode pour ≈Ωvaluer une comparaison
 	 * 
-	 * @return la véracité de la comparaison
+	 * @return la v≈Ωracit≈Ω de la comparaison
 	 */
 	public boolean[] evalComp(Comparaison c, Fait f){
 		double data [] = f.getData();
@@ -26,23 +26,22 @@ public class Condition {
 		return bool;
 	}
 	
-	/** Méthode pour évaluer la condition
+	/** M≈Ωthode pour ≈Ωvaluer la condition
 	 * 
-	 * @return la véracité de la condition
+	 * @return la v≈Ωracit≈Ω de la condition
 	 */
 	public boolean[] eval(Fait f){
 		double[] data = f.getData();
 		boolean b [] = new boolean[data.length];
-		//Problème : si la condition ne s'applique à rien, restera true
 		for(int k=0; k<b.length; k++){
 			b[k]=true;
 		}
 		boolean bc [];
 		//pour chaque comparaison dans la condition
 		for(int i = 0; i < this.comparaisons.length; i++){
-			//on évalue la véracité de la comparaison pour toutes les valeurs indiquées dans le fait
+			//on ≈Ωvalue la v≈Ωracit≈Ω de la comparaison pour toutes les valeurs indiqu≈Ωes dans le fait
 			bc = evalComp(this.comparaisons[i], f);
-			//puis pour chaque valeur on combine les résultats
+			//puis pour chaque valeur on combine les r≈Ωsultats
 			for(int j=0; j<data.length; j++){
 				b[j] = b[j] && bc[j];
 			}
