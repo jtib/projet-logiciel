98a697ec0f3a60aef0d0675900ad4788a1f72ca1
tib57 <juliette.tib@gmail.com>
2015-03-04 16:42:15 +0100

    Gestion des comparaisons

--- ProjetLog/src/Condition.java
+++ ProjetLog/src/Condition.java
@@ -1,34 +1,46 @@
 
 public class Condition {
-
+	
 	//remplacer par un vecteur
 	Comparaison[] comparaisons;
-
+	
 	//creer un nouveau
 	public Condition(Comparaison[] compa){
 		this.comparaisons = compa;
 	}
 	
 	//ajouter methode pour ajouter comparaison au vecteur comparaisons
-
-	/** Méthode pour évaluer une comparaison
+	
+	/** Methode pour evaluer une comparaison
 	 * 
 	 * @return la veracite de la comparaison
 	 */
 	public boolean[] evalComp(Comparaison c, Fait f){
 		double data [] = f.getData();
 		boolean bool [] = new boolean[data.length];
-		for(int i=0; i<data.length; i++){
+		for(boolean b : bool){
+			b = false;
+		}
+		//Si la comparaison s'applique à toutes les données
+		if(c.getElement(0) == "all"){
+			for(int i=0; i<data.length; i++){
+				double a = data[i];
+				c.setFirstArg(a);
+				bool[i] = c.eval();
+			}
+		}
+		//Sinon ne pas faire de for, évaluer seulement pour numéro concerné
+		else{
+			int i = Integer.valueOf(c.getElement(0));
 			double a = data[i];
 			c.setFirstArg(a);
-			bool[i] = c.eval();
-		}
+			bool[i] = c.eval();}
 		return bool;
 	}
-
-	/** Méthode pour évaluer la condition
+	
+	/** Methode pour evaluer la condition
 	 * 
-	 * @return la v≈Ωracit≈Ω de la condition
+	 * @return la veracite de la condition
 	 */
 	public boolean[] eval(Fait f){
 		double[] data = f.getData();
@@ -39,9 +51,9 @@ public class Condition {
 		boolean bc [];
 		//pour chaque comparaison dans la condition
 		for(int i = 0; i < this.comparaisons.length; i++){
-			//on Žvalue la veracite de la comparaison pour toutes les valeurs indiquees dans le fait
+			//on evalue la veracite de la comparaison pour le fait
 			bc = evalComp(this.comparaisons[i], f);
-			//puis pour chaque valeur on combine les r≈Ωsultats
+			//puis pour chaque valeur on combine les resultats
 			for(int j=0; j<data.length; j++){
 				b[j] = b[j] && bc[j];
 			}
