f564d58f7f5d6dace981f8a455815e3c3177fe18
tib57 <juliette.tib@gmail.com>
2015-03-04 18:59:32 +0100

    Ajout des classes pour comparaisons simples et doubles

---
+++ ProjetLog/src/ComparaisonDouble.java
@@ -0,0 +1,8 @@
+
+public class ComparaisonDouble extends Comparaison {
+	
+	public ComparaisonDouble(String arg1, String op, String arg2){
+		super(arg1, op, arg2);
+	}
+
+}
---
+++ ProjetLog/src/ComparaisonSimple.java
@@ -0,0 +1,8 @@
+
+public class ComparaisonSimple extends Comparaison {
+	
+	public ComparaisonSimple (String arg1, String op, String arg2){
+		super(arg1, op, arg2);
+	}
+
+}
