5e162918204cd652ccd9467251a27e7f0b82250a
tib57 <juliette.tib@gmail.com>
2015-03-04 15:12:52 +0100

    Adaptation à Hashtable

--- ProjetLog/src/Fait.java
+++ ProjetLog/src/Fait.java
@@ -1,11 +1,12 @@
+import java.util.Hashtable;
 import java.util.Vector;
 
 
 public class Fait {
-
+	
 	private String name;
 	private double[] data;
-
+	
 	public Fait(String nom, double[] donnees){
 		this.name = nom;
 		this.data = donnees;
@@ -19,17 +20,16 @@ public class Fait {
 		return this.name;
 	}
 	
-	public Vector<Pair<Integer,Integer>> aEffectuer(Vector<Regle> rules){
+	public Hashtable<Integer,Integer> aEffectuer(Vector<Regle> rules){
 		boolean[] b;
-		Vector<Pair<Integer,Integer>> listeActions = new Vector<Pair<Integer,Integer>>();
+		Hashtable<Integer,Integer> listeActions = new Hashtable<Integer,Integer>();
 		for(Regle r : rules){
 			Fait fait = r.getFait();
 			if(fait == this){
 				b = (r.getCondition().eval(this));
 				for(int i=0; i<data.length; i++){
 					if(b[i]){
-						Pair<Integer,Integer> paire = new Pair<Integer,Integer>(i,r.getAction());
-						listeActions.add(paire);
+						listeActions.put(i,r.getAction());
 					}
 				}
 			}
