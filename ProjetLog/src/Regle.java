4a3ef49fb5822c41ad03b83cd8ad67c9540694ce
tib57 <juliette.tib@gmail.com>
2015-03-04 15:13:36 +0100

    Adaptation à Hashtable

--- ProjetLog/src/Regle.java
+++ ProjetLog/src/Regle.java
@@ -1,11 +1,11 @@
-import java.util.Vector;
+import java.util.Hashtable;
 
 public class Regle {
-
+	
 	private Fait fact;
 	private Condition premise;
 	private int action;
-
+	
 	public Regle(Fait fait, Condition premisse, int numAction){
 		this.fact = fait;
 		this.premise = premisse;
@@ -23,23 +23,22 @@ public class Regle {
 	public int getAction(){
 		return this.action;
 	}
-
+	
 	/** Methode pour etablir la liste des numeros des actions a effectuer
 	 * 
 	 * @param facts Liste des faits auxquels appliquer les regles
 	 * @return la liste des actions a effectuer (sous forme de numeros)
 	 */
-	public Vector<Pair<Integer,Integer>> aEffectuer(Fait[] facts){
+	public Hashtable<Integer,Integer> aEffectuer(Fait[] facts){
 		boolean[] b;
-		Vector<Pair<Integer,Integer>> listeActions = new Vector<Pair<Integer,Integer>>();
+		Hashtable<Integer,Integer> listeActions = new Hashtable<Integer,Integer>();
 		for(Fait f : facts){
 			double[] data = f.getData();
 			if(this.fact == f){
 				b = (this.premise.eval(f));
 				for(int i=0; i<data.length; i++){
 					if(b[i]){
-						Pair<Integer,Integer> paire = new Pair<Integer,Integer>(i,this.action);
-						listeActions.add(paire);
+						listeActions.put(i,this.action);
 					}
 				}
 			}
