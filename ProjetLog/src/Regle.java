962ec4bb9d542a31046b07245e595e478aaee2d6
tib57 <juliette.tib@gmail.com>
2015-03-18 17:15:35 +0100

    Utilisation de Pair

--- ProjetLog/src/Regle.java
+++ ProjetLog/src/Regle.java
@@ -1,45 +1,52 @@
-import java.util.Hashtable;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Vector;
 
 public class Regle {
-	
+
 	private Fait fact;
 	private Condition premise;
 	private int action;
-	
+
 	public Regle(Fait fait, Condition premisse, int numAction){
 		this.fact = fait;
 		this.premise = premisse;
 		this.action = numAction;
 	}
-	
+
 	public Fait getFait(){
 		return this.fact;
 	}
-	
+
 	public Condition getCondition(){
 		return this.premise;
 	}
-	
+
 	public int getAction(){
 		return this.action;
 	}
-	
+
 	/** Methode pour etablir la liste des numeros des actions a effectuer
 	 * 
 	 * @param facts Liste des faits auxquels appliquer les regles
 	 * @return la liste des actions a effectuer (sous forme de numeros)
 	 */
-	public Hashtable<Integer,Integer> aEffectuer(Fait[] facts){
+	public List<Pair<Integer,List<Integer>>> aEffectuer(Vector<Fait> facts){
 		boolean[] b;
-		Hashtable<Integer,Integer> listeActions = new Hashtable<Integer,Integer>();
+		List<Pair<Integer,List<Integer>>> listeActions = new ArrayList<Pair<Integer,List<Integer>>>();	
 		for(Fait f : facts){
 			double[] data = f.getData();
 			if(this.fact == f){
 				b = (this.premise.eval(f));
 				for(int i=0; i<data.length; i++){
+					List<Integer> lesActions = new ArrayList<Integer>();
+					Pair<Integer,List<Integer>> paireSujetActions = new Pair<Integer,List<Integer>>(i,lesActions);
 					if(b[i]){
-						listeActions.put(i,this.action);
+						// i permet de retrouver à quoi s'applique l'action
+						//Si la condition s'applique, on ajoute l'action au vecteur d'actions du numéro i du fait
+						paireSujetActions.getValue().add(this.getAction());
 					}
+					listeActions.add(paireSujetActions);
 				}
 			}
 		}
