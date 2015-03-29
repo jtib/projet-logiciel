4b95b2d17db94c5ccf37bcbd75b1e92140bfb741
tib57 <juliette.tib@gmail.com>
2015-03-29 01:50:04 +0100

    Modifs pour Pennerath

--- ProjetLog/src/Condition.java
+++ ProjetLog/src/Condition.java
@@ -1,3 +1,6 @@
+//import java.util.ArrayList;
+import java.util.List;
+
 
 public class Condition {
 
@@ -16,7 +19,7 @@ public class Condition {
 	 * @return la veracite de la comparaison
 	 */
 	public boolean[] evalComp(Comparaison c, Fait f){
-		Class classe = c.getClass();
+		Class<? extends Comparaison> classe = c.getClass();
 		boolean[] bool = (classe.equals(ComparaisonSimple.class) ? evalCompSimple(c,f) : evalCompDouble(c,f));
 		return bool;
 	}
@@ -26,16 +29,15 @@ public class Condition {
 	 * @return la veracite de la comparaison
 	 */
 	public boolean[] evalCompSimple(Comparaison c, Fait f){
-		double data [] = f.getData();
-		boolean bool [] = new boolean[data.length];
+		List<Double> data = f.getData();
+		boolean bool [] = new boolean[data.size()];
 		for(boolean b : bool){
 			b = true;
 		}
-		System.out.print(c.getElement(0));
 		//Si la comparaison s'applique à toutes les données
 		if(c.getElement(0) == "all"){
-			for(int i=0; i<data.length; i++){
-				double a = data[i];
+			for(int i=0; i<data.size(); i++){
+				double a = data.get(i);
 				c.setArg(0,String.valueOf(a));
 				bool[i] = c.eval();
 			}
@@ -44,9 +46,14 @@ public class Condition {
 		//Sinon ne pas faire de for, évaluer seulement pour numéro concerné
 		else{
 			int i = Integer.valueOf(c.getElement(0));
-			double a = data[i];
+			double a = data.get(i);
+			//memoriser l'ancien argument
+			String tmp = c.getElement(0);
+			//mettre son remplacement
 			c.setArg(0,String.valueOf(a));
 			bool[i] = c.eval();
+			//remettre l'ancien numero
+			c.setArg(0,tmp);
 		}
 		return bool;
 	}
@@ -60,15 +67,15 @@ public class Condition {
 	 * @return la veracite de la comparaison
 	 */
 	public boolean[] evalCompDouble(Comparaison c, Fait f){
-		double data [] = f.getData();
-		boolean bool [] = new boolean[data.length];
+		List<Double> data = f.getData();
+		boolean bool [] = new boolean[data.size()];
 		for(boolean b : bool){
 			b = true;
 		}
 		int i = Integer.valueOf(c.getElement(0));
 		int j = Integer.valueOf(c.getElement(2));
-		double arg1 = data[i];
-		double arg2 = data[j];
+		double arg1 = data.get(i);
+		double arg2 = data.get(j);
 		c.setArg(0,String.valueOf(arg1));
 		c.setArg(2, String.valueOf(arg2));
 		bool[i] = c.eval();
@@ -80,8 +87,8 @@ public class Condition {
 	 * @return la veracite de la condition
 	 */
 	public boolean[] eval(Fait f){
-		double[] data = f.getData();
-		boolean b [] = new boolean[data.length];
+		List<Double> data = f.getData();
+		boolean b [] = new boolean[data.size()];
 		for(int k=0; k<b.length; k++){
 			b[k]=true;
 		}
@@ -91,7 +98,7 @@ public class Condition {
 			//on evalue la veracite de la comparaison pour le fait
 			bc = evalComp(this.comparaisons[i], f);
 			//puis pour chaque valeur on combine les resultats
-			for(int j=0; j<data.length; j++){
+			for(int j=0; j<data.size(); j++){
 				b[j] = b[j] && bc[j];
 			}
 		}
